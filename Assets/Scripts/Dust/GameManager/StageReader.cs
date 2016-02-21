88c1b03e133a6cc4c4f0b35420de6c2a7d750b31
ralsoon <smallkirby423@yahoo.co.jp>
2016-02-21 11:21:35 +0900

    ゲームメイン追加

---
+++ Assets/Scripts/Dust/GameManager/StageReader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageReader : csvlord
+{
+    int stage;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    CSVRead();
+        stage = status[2];
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
