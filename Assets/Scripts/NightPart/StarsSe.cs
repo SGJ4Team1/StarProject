55893c024a0d9adbb0fb184cba16002599645df2
tunomizu <tumikihurai46@gmail.com>
2016-02-21 15:19:04 +0900

    夜パート変更

---
+++ Assets/Scripts/NightPart/StarsSe.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StarsSe : MonoBehaviour
+{
+    [Header("SE")]
+    public AudioClip audioClip;
+    AudioSource audioSource;
+
+    private bool SETr = true;
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (transform.GetComponent<Image>().color.a >= 255 && SETr == true)
+        {
+            SETr = false;
+            audioSource.Play();
+        }
+	}
+}
