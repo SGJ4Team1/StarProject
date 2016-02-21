d2ae2d59d08a7451f6ed8ce3ee0a13025c5b3801
wind1153squi <cwn83xdf.mt@gmail.com>
2016-02-21 15:22:02 +0900

    [Title] 音追加

--- Assets/Scripts/Tilte/Title.cs
+++ Assets/Scripts/Tilte/Title.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour {
+
+    public GameObject caption;
+    public GameObject logo;
+    public GameObject button;
 
 // private const :
 	
@@ -13,13 +18,13 @@ public class Title : MonoBehaviour {
 		false,
 		true,
 		true,
-		true,
 		false,
+        true,
         true,
 		false,
 		false,
     };
-    private GameObject caption;
+    
 	
 // private :
 	private int _step = 0;
@@ -28,7 +33,9 @@ public class Title : MonoBehaviour {
 	void Start () 
 	{
 		Debug.Log( "Title Part Start" );
-		caption = (GameObject)Resources.Load ("Title/Caption");
+        // タイトル画面用instance生成
+		logo = Instantiate(logo);
+        button = Instantiate(button);
     }
 	
 	// Update is called once per frame
@@ -45,13 +52,28 @@ public class Title : MonoBehaviour {
 					// タイトル画面からあらすじ
 					TitlePartAnimation script = GetComponent<TitlePartAnimation>();
 					script.ChangeRequest();
+                    // caption生成
 					caption = Instantiate( caption );
+                    // タイトル画面素材破棄
+                    Destroy(logo);
+                    Destroy(button);
 				}
 				break;
             case 10:
                 {
                     // Scene Change
                     Debug.Log("Title Part Finish");
+                    // caption破棄
+                    Destroy(caption);
+                    // 音停止
+                    GameObject obj = GameObject.Find("Sound");
+                    if (obj)
+                    {
+                        TitleSoundManager soundMgr = obj.GetComponent<TitleSoundManager>();
+                        soundMgr.StopAll();
+                    }
+                    // GameMainへ飛ぶ
+                    SceneManager.LoadScene("GameMain");
                 }
                 break;
 			default:
@@ -66,6 +88,14 @@ public class Title : MonoBehaviour {
 					{
 						TitlePartAnimation script = GetComponent<TitlePartAnimation>();
 						script.ChangeRequest();
+
+                        // ほんとはシングルトンすべきだろうけど…
+                        GameObject obj = GameObject.Find("Sound");
+                        if (obj)
+                        {
+                            TitleSoundManager soundMgr = obj.GetComponent<TitleSoundManager>();
+                            soundMgr.PlaySe(0, 0.9f);
+                        }
 					}
 				}
 				break;
---
+++ Assets/Scripts/Tilte/TitleSoundManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSoundManager : MonoBehaviour {
+
+    // public
+    public AudioClip se0;
+    public AudioClip bgm;
+
+    // private 
+    private AudioSource[] sources;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("se start");
+        sources = gameObject.GetComponents<AudioSource>();
+        sources[0].clip = bgm;
+        sources[0].Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlaySe(int index, float time = 0.0f)
+    {
+        sources[1].clip = se0;
+        sources[1].time = time;
+        sources[1].Play();
+    }
+
+    public void StopAll()
+    {
+        sources[0].Stop();
+        sources[1].Stop();
+    }
+}
